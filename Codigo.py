7fd2fce7a45e0697e3b11ff35dfac315b0bf3d4e
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-10-28 21:59:26 -0600

    Agregue la funcion

--- Codigo.py
+++ Codigo.py
@@ -1,3 +1,56 @@
 from plyer import notification
 import time
 import datetime
+
+
+def notificacion(segundos,separador):
+    minutos=segundos*60
+    time.sleep(minutos)
+    
+    
+    notification.notify(
+    title="NOTIFICACION",
+    message = "Metete al script para renovarlo :) ",
+    timeout=10,
+    )
+    notification.notify(
+    title="NOTIFICACION",
+    message = "¿Ya Hiciste tu tarea? :( ",
+    timeout=15,
+    )
+    
+    print("1=SI\n2=NO")
+    notifi=int(input("Quieres seguir esperando la notificacion : "))
+    print(separador)
+    
+    if notifi==2:
+        notification.notify(
+        title="NOTIFICACION",
+        message = "Espero que tu tarea , ya es este hecha Adioss... :) ",
+        timeout=15,
+        )
+        
+    while notifi==1:
+        time.sleep(minutos)
+        notification.notify(
+        title="NOTIFICACION",
+        message = "¿Ya Hiciste tu tarea ?:( ",
+        timeout=15,
+        )
+        
+        notification.notify(
+        title="NOTIFICACION",
+        message = "Metete al script para renovarlo :) ",
+        timeout=10,
+        )
+        
+        print("1=SI\n2=NO")
+        notifi=int(input("Quieres seguir esperando la notificacion : "))
+        print(separador)
+        
+        if notifi==2:
+            notification.notify(
+            title="NOTIFICACION",
+            message = "Espero que tu tarea , ya es este hecha Adioss... :) ",
+            timeout=15,
+            )
